63a072bb5860a6464c25222a8b66ecfab7b2a035
Nyakun <lummer333@rambler.ru>
2010-02-08 13:36:16 -0700

    Fixed image sending

--- kaishi.py
+++ kaishi.py
@@ -11,7 +11,7 @@
 __author__ = 'Trevor "tj9991" Slocum'
 __license__ = 'GNU GPL v3'
 
-import md5
+import hashlib
 import time
 import base64
 import sys
@@ -20,6 +20,7 @@ import zlib
 import pickle
 import thread
 import socket
+import time
 
 from funcs import toEntity
 
@@ -37,6 +38,7 @@ class kaishi(object):
     self.host = urllib.urlopen('http://ip.paq.cc/').read()
     self.port = 44545
     self.peerid = self.host + ':' + str(self.port)
+    self.packets = []
 
     # function hooks
     self.handleIncomingData = None
@@ -47,7 +49,14 @@ class kaishi(object):
   def start(self):
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.socket.bind(('', self.port))
-    self.socket.settimeout(5)
+    self.socket.settimeout(1) #non-blocking mode
+
+    self.streamSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.streamSocket.bind(('', self.port+1))
+    self.streamSocket.settimeout(1) #non-blocking mode
+    self.streamSocket.listen(5) #queued connections count
+
+    thread.start_new_thread(self.receiveStreamData, ())
     thread.start_new_thread(self.receiveData, ())
     thread.start_new_thread(self.pingAllPeers, ())
     thread.start_new_thread(self.pingProvider, ())
@@ -74,16 +83,29 @@ class kaishi(object):
     message = message.replace('\n', '').strip()
 
     data = ':'.join([str(self.protocol_version), identifier, bounce, uid, self.encodeTransitSafePeerID(args['origin']), message])
+
+    oldlen = len(data)
     data = zlib.compress(unicode(data), 9)
+
+    print 'Sending data, length: '+str(oldlen)+ ' compressed to: ' + str(len(data))
 
     if args['to']:
       recipients = [args['to']]
     else:
       recipients = self.peers
 
-    for peer in recipients: 
+    for peer in recipients:
       try:
-        self.socket.sendto(data, self.peerIDToTuple(peer))
+        if len(data) > 1024:
+          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          host, port = peer.rsplit(':', 1)
+          if host.startswith('['):
+            host = host[1:len(host)-1]
+          s.connect((host, int(port)+1))
+          s.sendall(data)
+          s.close()
+        else:
+          self.socket.sendto(data, self.peerIDToTuple(peer))
       except:
         # something went wrong, drop the peer
         self.debugMessage('Dropping ' + peer + ' due to a connection error')
@@ -92,56 +114,95 @@ class kaishi(object):
           return False
     return True
 
-  def receiveData(self):
+  def parseData(self, data, address):
+    bouncer_peerid = address[0] + ':' + str(address[1]) # peerid of the last bounce
+    protocol_version, identifier, bounce, uid, origin, message = data.split(':', 5)
+    message = toEntity(message)
+    if len(message) > 719530: # drop messages greater than 512*3/4 + 4*5 K
+      print 'Message dropped'
+      return
+    data = ':'.join([protocol_version, identifier, bounce, uid, origin, message])
+
+    peerid = self.decodeTransitSafePeerID(origin) # peerid which sent the original message
+    if data and uid not in self.uidlist:
+      if peerid not in self.peers and identifier != 'JOIN' and identifier != 'DROP':
+        self.addPeer(peerid)
+        self.debugMessage('Adding ' + peerid + ' from outside message')
+
+      if identifier == 'JOIN': # a user requests that they join the network
+        self.addPeer(peerid)
+        self.setPeerNickname(peerid, message) # add the nick sent in the JOIN message
+        self.sendData('PEERS', self.makePeerList(), to=peerid, bounce=False)
+      #elif identifier == 'IMGR': # a user requests an image
+        #self.sendData(identifier, message, uid=uid, origin=peerid)
+      #elif identifier == 'IMGA': # a user sent us an image
+        #self.saveImg
+      elif identifier == 'PEERS': # list of connected peers
+        try:
+          peers = pickle.loads(message)
+          [self.addPeer(peer, peer_nick) for peer, peer_nick in peers.items()]
+          self.debugMessage('Got peerlist from ' + peerid)
+        except:
+          pass
+      elif identifier == 'DROP':
+        self.dropPeer(peerid)
+      elif identifier == 'PING':
+        if peerid in self.pings:
+          self.pings.update({peerid: time.time()})
+        self.debugMessage('Got PING from ' + peerid)
+      elif identifier == 'NICK':
+        self.setPeerNickname(peerid, message)
+      else:
+        self.handleIncomingData(peerid, identifier, uid, message)
+      if bounce == '1':
+        self.sendData(identifier, message, uid=uid, origin=peerid)
+    elif data:
+      self.debugMessage('Not rerouting data: ' + data)
+
+  def receiveStreamData(self):
     while 1:
-      data = None
+      data = ''
       try:
-        data, address = self.socket.recvfrom(65536)
+        conn, address = self.streamSocket.accept()
+        while 1:
+          tmpData = conn.recv(65536)
+          if len(tmpData) == 0:
+            break
+          data += tmpData
+        conn.close()
+
+        oldlen = len(data)
         data = zlib.decompress(data)
 
-        bouncer_peerid = address[0] + ':' + str(address[1]) # peerid of the last bounce
-        protocol_version, identifier, bounce, uid, origin, message = data.split(':', 5)
-        message = toEntity(message)
-        data = ':'.join([protocol_version, identifier, bounce, uid, origin, message])
+#        print 'Received stream, length: ' + str(len(data)) + ' compressed to: ' + str(oldlen)
 
-        peerid = self.decodeTransitSafePeerID(origin) # peerid which sent the original message
+        self.parseData(data, address)
       except socket.timeout:
         pass
       except:
         self.debugMessage('Failed to establish a connection.')
         pass
+      if data == '':
+        data = None
 
-      if data and uid not in self.uidlist:
-        if peerid not in self.peers and identifier != 'JOIN' and identifier != 'DROP':
-          self.addPeer(peerid)
-          self.debugMessage('Adding ' + peerid + ' from outside message')
+  def receiveData(self):
+    while 1:
+      data = None
+      try:
+        data, address = self.socket.recvfrom(65536)
 
-        if identifier == 'JOIN': # a user requests that they join the network
-          self.addPeer(peerid)
-          self.setPeerNickname(peerid, message) # add the nick sent in the JOIN message
-          self.sendData('PEERS', self.makePeerList(), to=peerid, bounce=False)
-        elif identifier == 'PEERS': # list of connected peers
-          try:
-            peers = pickle.loads(message)
-            [self.addPeer(peer, peer_nick) for peer, peer_nick in peers.items()]
-            self.debugMessage('Got peerlist from ' + peerid)
-          except:
-            pass
-        elif identifier == 'DROP':
-          self.dropPeer(peerid)
-        elif identifier == 'PING':
-          if peerid in self.pings:
-            self.pings.update({peerid: time.time()})
-          self.debugMessage('Got PING from ' + peerid)
-        elif identifier == 'NICK':
-          self.setPeerNickname(peerid, message)
-        else:
-          self.handleIncomingData(peerid, identifier, uid, message)
+        oldlen = len(data)
+        data = zlib.decompress(data)
 
-        if bounce == '1':
-          self.sendData(identifier, message, uid=uid, origin=peerid)
-      elif data:
-        self.debugMessage('Not rerouting data: ' + data)
+#        print 'Received datagr, length: ' + str(len(data)) + ' compressed to: ' + str(oldlen)
+
+        self.parseData(data, address)
+      except socket.timeout:
+        data = None
+        pass
+      except:
+        self.debugMessage('Failed to establish a connection.')
+        pass
 
   def addPeer(self, peerid, peer_nick=''):
     result = False
@@ -270,7 +331,7 @@ class kaishi(object):
 
   @staticmethod
   def makeID(data):
-    m = md5.new()
+    m = hashlib.md5()
     m.update(str(time.time()))
     m.update(str(data))
     return base64.encodestring(m.digest())[:-3].replace('/', '$')
